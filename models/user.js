25933fc643ba2efdf17a8157098cc4377bd2eea7
Nate Deakers <ndeakers@gmail.com>
2021-05-17 16:50:44 -0700

    ran tests and fixed user models

--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
 "use strict";
 
-const {NotFoundError} = require('../expressError');
+const { NotFoundError } = require('../expressError');
 const Message = require('./message');
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config.js");
+const db = require("../db");
 /** User of the site. */
 
 class User {
@@ -14,9 +17,9 @@ class User {
     const hashedPassword = await bcrypt.hash(
       password, BCRYPT_WORK_FACTOR);
 
-    const result = db.query(
-      `INSERT INTO users (username, password, first_name, last_name, phone)
-          VALUES ($1, $2, $3, $4, $5)
+    const result = await db.query(
+      `INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
+          VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
           RETURNING username, password, first_name, last_name, phone`,
       [username, hashedPassword, first_name, last_name, phone]
     )
@@ -29,7 +32,7 @@ class User {
    */
 
   static async authenticate(username, password) {
-    const result = db.query(
+    const result = await db.query(
       `SELECT password
           FROM users
           WHERE username = $1`,
@@ -49,16 +52,16 @@ class User {
    */
 
   static async updateLoginTimestamp(username) {
-    db.query(`Update users
-                SET last_login_at = (current_timestamp)
-                WHERE user = $1`, [username]);
+    await db.query(`Update users
+                SET last_login_at=current_timestamp
+                WHERE username = $1`, [username]);
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
-    const results = db.query(`
+    const results = await db.query(`
       SELECT username, first_name, last_name
           FROM users`);
 
@@ -75,14 +78,14 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
-    const result = db.query(`
+    const result = await db.query(`
       SELECT username, first_name, last_name, phone, join_at, last_login_at
           FROM users
           WHERE username = $1`, [username]);
 
-    const user = result.rows[0];    
+    const user = result.rows[0];
 
-    if(!user) {
+    if (!user) {
       throw new NotFoundError(`Not Found ${username}`);
     }
     return user
@@ -97,55 +100,52 @@ class User {
    */
 
   static async messagesFrom(username) {
-    const results = db.query(`
-      SELECT id
-      FROM messages
-      WHERE from_user = $1`, [username]);
-    
-    const messageIDs = results.rows[0];
+    const results = await db.query(`
+    SELECT id
+    FROM messages
+    WHERE from_username = $1`, [username]);
 
-    const messagesData = messageIDs.map( id => Message.get(id));
+    const messageIDs = results.rows;
+    console.log("reults.rows for messagesTo", messageIDs);
 
-    const messageResponses = messagesData.map(m => 
-      {
-        m.id, 
-        m.to_user,
-        m.body, 
-        m.sent_at, 
-        m.read_at
-      }); 
+    const messagesP = messageIDs.map(m => Message.get(m.id));
+    const messagesData = await Promise.all(messagesP);
+    console.log("messagesData", messagesData);
 
-      return messageResponses;
+    const messageResponses = messagesData.map(m => ({
+      id: m.id,
+      to_user: m.to_user,
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at
+    }));
+
+    return messageResponses;
   }
 
-  /** Return messages to this user.
-   *
-   * [{id, from_user, body, sent_at, read_at}]
-   *
-   * where from_user is
-   *   {id, first_name, last_name, phone}
-   */
 
   static async messagesTo(username) {
-    const results = db.query(`
+    const results = await db.query(`
       SELECT id
       FROM messages
-      WHERE from_user = $1`, [username]);
-    
-    const messageIDs = results.rows[0];
+      WHERE to_username = $1`, [username]);
 
-    const messagesData = messageIDs.map( id => Message.get(id));
+    const messageIDs = results.rows;
+    console.log("reults.rows for messagesTo", messageIDs);
 
-    const messageResponses = messagesData.map(m => 
-      {
-        m.id, 
-        m.from_user,
-        m.body, 
-        m.sent_at, 
-        m.read_at
-      }); 
+    const messagesP = messageIDs.map(m => Message.get(m.id));
+    const messagesData = await Promise.all(messagesP);
+    console.log("messagesData", messagesData);
 
-      return messageResponses;
+    const messageResponses = messagesData.map(m => ({
+      id: m.id,
+      from_user: m.from_user,
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at
+    }));
+
+    return messageResponses;
   }
 }
 
