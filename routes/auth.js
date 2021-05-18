f2a7583d6f46598e8957a3735102ab9253507a23
Kiru <kirushanth.ravindran@gmail.com>
2021-05-17 17:16:45 -0700

    finished auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,41 @@
 "use strict";
 
+
 const Router = require("express").Router;
 const router = new Router();
+const { User } = require('../models/user');
+const jwt = require("jsonwebtoken");
+const {SECRET_KEY} = require('../config');
+const {UnauthorizedError, BadRequestError} = require('../expressError');
+
 
 /** POST /login: {username, password} => {token} */
+router.post('/login', async function (req, res, next) {
+  const { username, password } = req.body;
+  const authenticated = await User.authenticate(username, password);
 
+  if(authenticated) {
+    const token = jwt.sign({username}, SECRET_KEY);
+    User.updateLoginTimestamp(username);
+    return res.json({token});
+  }
+  throw new UnauthorizedError("Invalid Username or Password");
+})
 
 /** POST /register: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+
+ router.post('/register', async function (req, res, next) {
+  const { username, password, first_name, last_name, phone } = req.body;
+  const user = await User.register(username, password, first_name, last_name, phone);
+
+  if (user){
+    const token = jwt.sign({username: user.username}, SECRET_KEY);
+    return res.json({token});
+  }
+  throw new BadRequestError("Invalid registration information");
+})
 
 module.exports = router;
